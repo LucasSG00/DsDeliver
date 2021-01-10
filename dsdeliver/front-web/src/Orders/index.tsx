2f840bc82bf25eb9fc319f0a2daa23c51d379bb7
LucasSG00 <lucas_s_goncalves@hotmail.com>
2021-01-09 22:03:56 -0300

    Criação dos mapa para localização do pedido

--- dsdeliver/front-web/src/Orders/index.tsx
+++ dsdeliver/front-web/src/Orders/index.tsx
@@ -2,12 +2,14 @@ import { ProductsList } from './ProductsList';
 import { StepsHeader } from './StepsHeader';
 import { useEffect, useState } from 'react';
 import './styles.css';
-import { Product } from './types';
+import { OrderLocationdata, Product } from './types';
 import { fetchProducts } from './api';
+import { OrderLocation } from './OrderLocation';
 
 export function Orders(){
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLocation] = useState<OrderLocationdata>();
     
     useEffect(() => {
         fetchProducts()
@@ -19,6 +21,7 @@ export function Orders(){
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     );
 }
